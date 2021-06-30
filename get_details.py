f3a976536be608c843f6df0625a13edd49cca207
Prathmesh <52091202+patrickn699@users.noreply.github.com>
2021-06-30 19:21:52 +0530

    Add files via upload

---
+++ get_details.py
@@ -0,0 +1,52 @@
+from bs4 import BeautifulSoup as bs
+import urllib
+from pprint import pprint
+import requests
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import time
+import pandas as pd
+
+
+
+    
+
+def fetch( numb):
+
+    options = webdriver.ChromeOptions()
+    #options.add_argument('headless')
+    chrome_path = r'E:/Chrome Downloads/chromedriver_win32/chromedriver.exe'
+    options.binary_location = r"C:/Program Files/Google/Chrome/Application/chrome.exe"
+    driver = webdriver.Chrome(executable_path=chrome_path,chrome_options=options)
+
+    num = 'MH14BK7830'
+    #pas = 'vehicledetails@1'
+    #bro = webdriver.Chrome()
+    #url = 'https://vahan.nic.in/nrservices/faces/user/citizen/citizenlogin.xhtml'
+    #url1 = 'https://vahaninfos.com/vehicle-details-by-number-plate'
+    url2 = 'https://www.drivespark.com/rto-vehicle-registration-details/'
+    
+    try:
+        driver.get(url2)
+        time.sleep(10)
+        print(numb)
+
+        box = driver.find_element_by_xpath('//*[@id="reg_num"]')
+        box.send_keys(numb)
+        #WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="fuelcalculatebtn"]')))
+        driver.find_element_by_xpath('//*[@id="fuelcalculatebtn"]').click()
+        #time.sleep(15)
+        get_url = driver.current_url
+        #pg = driver.page_source(get_url)
+        #print(get_url)
+        ds = pd.read_html(get_url)
+        
+        ds = ds[1]
+        print(ds)
+        return ds
+
+    except Exception as e:
+        print(e)
