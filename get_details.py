4becf6ee0a7c0a97d82870352ac9e329b3fd39ee
patrickn699 <52091202+patrickn699@users.noreply.github.com>
2021-08-13 13:45:49 +0530

    get details updated

--- get_details.py
+++ get_details.py
@@ -15,6 +15,11 @@ def fetch( numb):
 
     options = webdriver.ChromeOptions()
     #options.add_argument('headless')
+    #options.add_argument('headless')
+    #options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+    #options.add_argument('--no-sandbox')
+    #options.add_argument('--disable-dev-shm-usage')
     chrome_path = r'E:/Chrome Downloads/chromedriver_win32/chromedriver.exe'
     options.binary_location = r"C:/Program Files/Google/Chrome/Application/chrome.exe"
     driver = webdriver.Chrome(executable_path=chrome_path,chrome_options=options)
